679f75aa2e5734916bb021092c3d26fd409ebeb4
d9m <kumagai@sencorp.co.jp>
2014-03-04 06:30:07 +0900

    resolve yum install

--- site-cookbooks/gui-devel-env/recipes/desktop.rb
+++ site-cookbooks/gui-devel-env/recipes/desktop.rb
@@ -1,5 +1,9 @@
 # install desktop environment
-execute "yum -y groupinstall x11 fonts basic-desktop desktop-platform \"Japanese Support\""
+execute "yum -y groupinstall x11"
+execute "yum -y groupinstall fonts"
+execute "yum -y groupinstall basic-desktop"
+execute "yum -y groupinstall desktop-platform"
+execute "yum -y groupinstall \"Japanese Support\""
 execute "yum -y install glibc"
 
 execute "service messagebus start"
