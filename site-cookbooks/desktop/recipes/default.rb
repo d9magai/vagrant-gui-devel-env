27944f595280a93f5640103d643874d58f83d71b
d9magai <kumagai@sencorp.co.jp>
2014-06-20 11:40:51 +0900

    create desktop cookbook

---
+++ site-cookbooks/desktop/recipes/default.rb
@@ -0,0 +1,23 @@
+# install desktop environment
+[
+"x11",
+"fonts",
+"basic-desktop",
+"desktop-platform",
+"\"JapaneseSupport\"",
+].each do |group|
+  execute "yum -y groupinstall #{group}"
+end
+
+%w{messagebus haldaemon}.each do |service_name|
+  service service_name do
+    action   [ :enable, :start ]
+  end
+end
+
+template "/etc/inittab" do
+  owner "root"
+  group "root"
+  mode 0644
+end
+
