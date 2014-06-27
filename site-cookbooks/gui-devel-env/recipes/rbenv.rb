cc30a943a2e0ef90f0256868f2a2198dd76c72a1
d9magai <kumagai@sencorp.co.jp>
2014-06-27 12:19:11 +0900

    install only rubygems-update, rails, rspec

--- site-cookbooks/gui-devel-env/recipes/rbenv.rb
+++ site-cookbooks/gui-devel-env/recipes/rbenv.rb
@@ -65,7 +65,7 @@ execute "append to gemrc" do
 end
 
 
-execute "install rubygems-update" do
+execute "install rubygems-update, rails" do
   user  node['user']
   group  node['group']
 
@@ -73,187 +73,12 @@ execute "install rubygems-update" do
   command <<-EOH
         #{node['home']}/.rbenv/shims/gem install rubygems-update
         #{node['home']}/.rbenv/shims/update_rubygems
-        #{node['home']}/.rbenv/shims/gem pristine --all
-  EOH
-  not_if { File.exist?("#{node['home']}/.rbenv/shims/update_rubygems") }
-  action :run
-end
-
-# install rails
-bash "install rails" do
-  user  node['user']
-  group  node['group']
-
-  environment 'HOME' => "#{node['home']}"
-  code <<-EOC
-        #{node['home']}/.rbenv/shims/gem sources -a http://gems.rubyforge.org/
         #{node['home']}/.rbenv/shims/gem install rails
-   EOC
-  
-  not_if { File.exist?("#{node['home']}/.rbenv/shims/rails") }
-  action :run
-end
-
-# install ruby-debug-ide
-bash "install ruby-debug-ide" do
-  user  node['user']
-  group  node['group']
-
-  environment 'HOME' => "#{node['home']}"
-  code <<-EOC
-        #{node['home']}/.rbenv/shims/gem install ruby-debug-ide
-   EOC
-  
-  not_if { File.exist?("#{node['home']}/.rbenv/shims/rdebug-ide") }
-  action :run
-end
-
-# install rspec
-bash "install rspec" do
-  user  node['user']
-  group  node['group']
-
-  environment 'HOME' => "#{node['home']}"
-  code <<-EOC
         #{node['home']}/.rbenv/shims/gem install rspec
-   EOC
-  
-  not_if { File.exist?("#{node['home']}/.rbenv/shims/rspec") }
-  action :run
-end
-
-# install sdoc
-bash "install sdoc" do
-  user  node['user']
-  group  node['group']
-
-  environment 'HOME' => "#{node['home']}"
-  code <<-EOC
-        #{node['home']}/.rbenv/shims/gem install sdoc
-   EOC
-  
-  not_if { File.exist?("#{node['home']}/.rbenv/shims/sdoc") }
-  action :run
-end
-
-# install therubyracer
-bash "install therubyracer" do
-  user  node['user']
-  group  node['group']
-
-  environment 'HOME' => "#{node['home']}"
-  code <<-EOC
-        #{node['home']}/.rbenv/shims/gem install therubyracer
-   EOC
-  
-  not_if "#{node['home']}/.rbenv/shims/gem list | grep therubyracer"
-  action :run
-end
-
-# install coffee-rails
-bash "install coffee-rails" do
-  user  node['user']
-  group  node['group']
-
-  environment 'HOME' => "#{node['home']}"
-  code <<-EOC
-        #{node['home']}/.rbenv/shims/gem install coffee-rails
-   EOC
-  
-  not_if "#{node['home']}/.rbenv/shims/gem list | grep coffee-rails"
-  action :run
-end
-
-# install sqlite3
-bash "install sqlite3" do
-  user  node['user']
-  group  node['group']
-
-  environment 'HOME' => "#{node['home']}"
-  code <<-EOC
-        #{node['home']}/.rbenv/shims/gem install sqlite3
-   EOC
-  
-  not_if "#{node['home']}/.rbenv/shims/gem list | grep sqlite3"
-  action :run
-end
-
-# install sass-rails
-bash "install sass-rails" do
-  user  node['user']
-  group  node['group']
-
-  environment 'HOME' => "#{node['home']}"
-  code <<-EOC
-        #{node['home']}/.rbenv/shims/gem install sass-rails
-   EOC
-  
-  not_if "#{node['home']}/.rbenv/shims/gem list | grep sass-rails"
-  action :run
-end
-
-bash "install uglifier" do
-  user  node['user']
-  group  node['group']
-
-  environment 'HOME' => "#{node['home']}"
-  code <<-EOC
-        #{node['home']}/.rbenv/shims/gem install uglifier
-   EOC
-  
-  not_if "#{node['home']}/.rbenv/shims/gem list | grep uglifier"
-  action :run
-end
-
-bash "install jquery-rails" do
-  user  node['user']
-  group  node['group']
-
-  environment 'HOME' => "#{node['home']}"
-  code <<-EOC
-        #{node['home']}/.rbenv/shims/gem install jquery-rails
-   EOC
-  
-  not_if "#{node['home']}/.rbenv/shims/gem list | grep jquery-rails"
-  action :run
-end
-
-bash "install turbolinks" do
-  user  node['user']
-  group  node['group']
-
-  environment 'HOME' => "#{node['home']}"
-  code <<-EOC
-        #{node['home']}/.rbenv/shims/gem install turbolinks
-   EOC
-  
-  not_if "#{node['home']}/.rbenv/shims/gem list | grep turbolinks"
-  action :run
-end
-
-bash "install jbuilder -v 1.2" do
-  user  node['user']
-  group  node['group']
-
-  environment 'HOME' => "#{node['home']}"
-  code <<-EOC
-        #{node['home']}/.rbenv/shims/gem install jbuilder -v 1.2
-   EOC
-  
-  not_if "#{node['home']}/.rbenv/shims/gem list | grep jbuilder"
-  action :run
-end
-
-bash "install execjs" do
-  user  node['user']
-  group  node['group']
-
-  environment 'HOME' => "#{node['home']}"
-  code <<-EOC
-        #{node['home']}/.rbenv/shims/gem install execjs
-   EOC
-  
-  not_if "#{node['home']}/.rbenv/shims/gem list | grep execjs"
+        #{node['home']}/.rbenv/shims/gem pristine --all
+        #{node['home']}/.rbenv/bin/rbenv rehash
+  EOH
+  not_if { File.exist?("#{node['home']}/.rbenv/shims/rails") }
   action :run
 end
 
