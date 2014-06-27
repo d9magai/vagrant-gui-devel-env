806fbbcd7a8b7f0cdf16a45dd23f61d7a17e5c2a
d9magai <kumagai@sencorp.co.jp>
2014-06-27 12:18:27 +0900

    take away lvm2-monitor from disabe services

--- site-cookbooks/gui-devel-env/recipes/basepackages.rb
+++ site-cookbooks/gui-devel-env/recipes/basepackages.rb
@@ -20,7 +20,7 @@ end
 # auditd - SELinux logger
 # netfs - NFS client
 %w{auditd  autofs  avahi-daemon  bluetooth  cups  firstboot
-   gpm  hidd  isdn  ip6tables kudzu  lvm2-monitor  mcstrans 
+   gpm  hidd  isdn  ip6tables kudzu  mcstrans 
    mdmonitor netfs  nfslock  pcscd  portmap
    rawdevices  restorecond  rpcgssd  rpcidmapd  smartd  xfs
    yum-updatesd 
